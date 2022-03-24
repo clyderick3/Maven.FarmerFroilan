ae6ba7b1f092e9ffdfaaa7efbd886bc649b1b754
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-03-24 10:20:42 -0400

    made some generic changes and made folders

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal.java
@@ -1,11 +1,4 @@
 package com.zipcodewilmington.froilansfarm;
 
-public abstract class Animal<T extends Edible> implements NoiseMaker, Eater<T>{
-    public boolean eat(T food) {
-        return false;
-    }
-
-    public String makeNoise() {
-        return null;
-    }
+public abstract class Animal implements NoiseMaker, Eater{
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Person.java
@@ -2,4 +2,8 @@ package com.zipcodewilmington.froilansfarm;
 
 public class Person extends Animal{
 
+    @Override
+    public String makeNoise() {
+        return "Argh";
+    }
 }
