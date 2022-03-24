7a92dabeca0f35490585b0b1dbb6357527021bf4
meredith <meredithbrown302@gmail.com>
2022-03-24 15:07:59 -0400

    merged duplicate Botanist and Farmer classes

--- src/main/java/com/zipcodewilmington/froilansfarm/Animals/Botanist.java
+++
@@ -1,8 +0,0 @@
-package com.zipcodewilmington.froilansfarm.Animals;
-
-import com.zipcodewilmington.froilansfarm.Plants.Crop;
-import com.zipcodewilmington.froilansfarm.ShelterAndStorage.CropRow;
-
-public interface Botanist {
-    <T extends Crop> boolean plant(T plantType, CropRow cropRow);
-}
