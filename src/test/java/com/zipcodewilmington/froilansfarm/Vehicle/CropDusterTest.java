632fe1fc728e54cf8021bcab301daf63e8599732
meredith <meredithbrown302@gmail.com>
2022-03-26 13:23:41 -0400

    commit before push

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Vehicle/CropDusterTest.java
@@ -0,0 +1,18 @@
+package com.zipcodewilmington.froilansfarm.Vehicle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CropDusterTest {
+
+    @Test
+    void fertilize() {
+    }
+
+    @Test
+    void operate() {
+        // test input person not pilot
+        // test input person pilot
+    }
+}
