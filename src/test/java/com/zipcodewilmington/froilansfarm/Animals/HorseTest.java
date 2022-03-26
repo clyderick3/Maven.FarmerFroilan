fd37b97bda5f4c9d2521f9a490e8d141075675b2
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-03-26 10:27:23 -0400

    chicken test

--- src/test/java/com/zipcodewilmington/froilansfarm/Animals/HorseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animals/HorseTest.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.froilansfarm.Animals;
 
+import com.zipcodewilmington.froilansfarm.Food.Tomato;
+import com.zipcodewilmington.froilansfarm.Plants.TomatoPlant;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,12 @@ class HorseTest {
     void makeNoise() {
         Horse horse = new Horse();
         Assert.assertEquals("Neigh", horse.makeNoise());
+    }
+
+    @Test
+    void eatInterfaceTest(){
+        Horse horse = new Horse();
+        Tomato tomato = new Tomato();
+        Assert.assertTrue(horse.eat(tomato));
     }
 }
