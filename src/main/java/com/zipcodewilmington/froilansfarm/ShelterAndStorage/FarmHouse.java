b0ad30f88684438332e85edd126bfb446584bf95
Clyde Broderick <clyderick3@gmail.com>
2022-03-27 13:40:35 -0400

    Commit before pull.

--- src/main/java/com/zipcodewilmington/froilansfarm/ShelterAndStorage/FarmHouse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ShelterAndStorage/FarmHouse.java
@@ -5,4 +5,7 @@ import com.zipcodewilmington.froilansfarm.Animals.Person.Person;
 public class FarmHouse extends Shelter<Person> {
 
     //FarmHouse contains animals like a shelter.
+    //Create an ArrayList for the people that live in the farmhouse plus a constructor.
+
+
 }
