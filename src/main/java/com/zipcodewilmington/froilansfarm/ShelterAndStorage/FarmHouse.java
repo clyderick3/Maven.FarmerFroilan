fab141baa8f34ed2d62b40411dddd77ff54f1e88
Clyde Broderick <clyderick3@gmail.com>
2022-03-27 22:54:13 -0400

    Added ArrayList to FarmHouse.

--- src/main/java/com/zipcodewilmington/froilansfarm/ShelterAndStorage/FarmHouse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ShelterAndStorage/FarmHouse.java
@@ -2,10 +2,17 @@ package com.zipcodewilmington.froilansfarm.ShelterAndStorage;
 
 import com.zipcodewilmington.froilansfarm.Animals.Person.Person;
 
-public class FarmHouse extends Shelter<Person> {
+import java.util.ArrayList;
 
+public class FarmHouse {
+
+    //Removed Farmhouse "extends Shelter<Person>
     //FarmHouse contains animals like a shelter.
     //Create an ArrayList for the people that live in the farmhouse plus a constructor.
 
+        ArrayList<Person> people;
+
+
+        }
 
 }
